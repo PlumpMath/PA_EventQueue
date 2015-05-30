414da8871db1fc997afd2396ba80fe5e4ee5b3d2
Putis <marek.28.93@gmail.com>
2015-05-30 23:33:00 +0200

    No more needed

--- app/Global.scala
+++ app/Global.scala
@@ -1,8 +1,5 @@
+import com.google.inject.{Guice, Injector}
 import play.api._
-import play.api.mvc._
-import play.api.http.HeaderNames._
-import play.api.libs.concurrent.Execution.Implicits._
-
 
 /**
  * Created by Wojtek on 29/05/15.
@@ -11,20 +8,5 @@ object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
     Logger.info("Application has started")
-  }
-
-  //     _______ _________          _______  _______
-  //    (  ____ \\__   __/|\     /|(       )(  ____ \
-  //    | (    \/   ) (   ( \   / )| () () || (    \/
-  //    | (__       | |    \ (_) / | || || || (__
-  //    |  __)      | |     ) _ (  | |(_)| ||  __)
-  //    | (         | |    / ( ) \ | |   | || (
-  //    | )      ___) (___( /   \ )| )   ( || (____/\
-  //    |/       \_______/|/     \||/     \|(_______/
-  //FIXME make it safer
-  override def doFilter(action: EssentialAction): EssentialAction = EssentialAction { request =>
-    action.apply(request).map(_.withHeaders(
-      ACCESS_CONTROL_ALLOW_ORIGIN -> "*"
-    ))
   }
 }
